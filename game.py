3a231e925752cd19010efbc7f394d8360fa83a05
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 12:51:52 -0700

    Count tries; print success

--- game.py
+++ game.py
@@ -8,14 +8,18 @@ def guessing_game():
     print("{}, I'm thinking of a number between 1 and 100.".format(user_name))
     print('Try to guess my number.')
 
-    guess = raw_input('Your guess:')
+    guess = raw_input('Your guess?:')
+    c = 1
 
     while guess != random_num:
         if guess > random_num:
-            guess = raw_input("Your guess is too high, try again.\nYour guess?")
+            guess = raw_input("Your guess is too high, try again.\nYour guess?:")
+            c += 1
         elif guess < random_num:
-            guess = raw_input("Your guess is too low, try again.\nYour guess?")
-    
+            guess = raw_input("Your guess is too low, try again.\nYour guess?:")
+            c += 1
+
+    print "Well done, {}! You found my number in {} tries!".format(user_name, c)
 
 
 
