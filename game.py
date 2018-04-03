691ee46993d216eb0740d02ee18972825f5dddf0
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 12:48:33 -0700

    User Guesses number

--- game.py
+++ game.py
@@ -5,6 +5,17 @@ def guessing_game():
     import random
     random_num = random.randint(1,100)
 
+    print("{}, I'm thinking of a number between 1 and 100.".format(user_name))
+    print('Try to guess my number.')
+
+    guess = raw_input('Your guess:')
+
+    while guess != random_num:
+        if guess > random_num:
+            guess = raw_input("Your guess is too high, try again.\nYour guess?")
+        elif guess < random_num:
+            guess = raw_input("Your guess is too low, try again.\nYour guess?")
     
-guessing_game()
+
+
 
