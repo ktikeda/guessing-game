a460ec033f889c16460f07f2647333a049c86e49
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 12:58:52 -0700

    Validate; typecast guess as int

--- game.py
+++ game.py
@@ -9,17 +9,22 @@ def guessing_game():
     print('Try to guess my number.')
 
     guess = raw_input('Your guess?:')
+    while not guess.isdigit():
+        guess = raw_input("Sorry, that is not a valid guess. Please choose a number between 1 and 100.:")
+    while int(guess) < 1 or int(guess) > 100:
+        guess = raw_input("Sorry, that is not a valid guess. Please choose a number between 1 and 100.:")
+    
     c = 1
 
-    while guess != random_num:
-        if guess > random_num:
+    while int(guess) != random_num:
+        if int(guess) > random_num:
             guess = raw_input("Your guess is too high, try again.\nYour guess?:")
             c += 1
-        elif guess < random_num:
+        elif int(guess) < random_num:
             guess = raw_input("Your guess is too low, try again.\nYour guess?:")
             c += 1
 
     print "Well done, {}! You found my number in {} tries!".format(user_name, c)
 
 
-
+guessing_game()
