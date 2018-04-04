21e1b97fe9c1c09442c4ae16273bbc7a37e03b52
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 17:22:33 -0700

    Added option of playing again using while loop

--- game.py
+++ game.py
@@ -3,28 +3,41 @@
 def guessing_game():
     user_name = raw_input("Hi! What's your name?:")
     import random
-    random_num = random.randint(1,100)
+    
+    playing = True
 
-    print("{}, I'm thinking of a number between 1 and 100.".format(user_name))
-    print('Try to guess my number.')
+    while playing:
 
-    total_tries = 0
-    guess = raw_input('Your guess?:')
-    validation_results = val_guess(guess, total_tries)
+        random_num = random.randint(1,100)
 
-    guess = validation_results[0]
-    total_tries = validation_results[1]
+        print("{}, I'm thinking of a number between 1 and 100.".format(user_name))
+        print('Try to guess my number.')
 
-    while guess != random_num:
-        if guess > random_num:
-            guess = raw_input("Your guess is too high, try again.\nYour guess?:")
-        elif guess < random_num:
-            guess = raw_input("Your guess is too low, try again.\nYour guess?:")
+        total_tries = 0
+        guess = raw_input('Your guess?:')
         validation_results = val_guess(guess, total_tries)
+
         guess = validation_results[0]
         total_tries = validation_results[1]
 
-    print "Well done, {}! You found my number in {} tries!".format(user_name, total_tries)
+        while guess != random_num:
+            if guess > random_num:
+                guess = raw_input("Your guess is too high, try again.\nYour guess?:")
+            elif guess < random_num:
+                guess = raw_input("Your guess is too low, try again.\nYour guess?:")
+            validation_results = val_guess(guess, total_tries)
+            guess = validation_results[0]
+            total_tries = validation_results[1]
+
+        print "Well done, {}! You found my number in {} tries!".format(user_name, total_tries)
+
+        play_again = raw_input("Would you like to play again? Enter 'Y' for 'yes' or 'N' for 'no.':")
+        while play_again.lower() != 'n' and play_again.lower() != 'y':
+            play_again = raw_input("Sorry, I didn't get that. Would you like to play again? Enter 'Y' for 'yes' or 'N' for 'no.':")
+        if play_again.lower() == 'y':
+            continue
+        elif play_again.lower() == 'n':
+            playing = False
 
 def val_guess(user_input, c_tries):
     while True:
